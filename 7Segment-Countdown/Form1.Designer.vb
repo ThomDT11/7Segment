5b18fc77c259d2f690a13e41e8ca396dc50263b5
Your Name <you@example.com>
2020-01-16 17:42:15 +0800

    Second Commit - Functioning

--- 7Segment-Countdown/Form1.Designer.vb
+++ 7Segment-Countdown/Form1.Designer.vb
@@ -22,9 +22,181 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.VI = New System.Windows.Forms.Label()
+        Me.II = New System.Windows.Forms.Label()
+        Me.VII = New System.Windows.Forms.Label()
+        Me.I = New System.Windows.Forms.Label()
+        Me.IV = New System.Windows.Forms.Label()
+        Me.V = New System.Windows.Forms.Label()
+        Me.III = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(357, 165)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.PictureBox1.Location = New System.Drawing.Point(32, 41)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(283, 438)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'VI
+        '
+        Me.VI.BackColor = System.Drawing.Color.RoyalBlue
+        Me.VI.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.VI.Location = New System.Drawing.Point(92, 115)
+        Me.VI.Name = "VI"
+        Me.VI.Size = New System.Drawing.Size(26, 101)
+        Me.VI.TabIndex = 2
+        '
+        'II
+        '
+        Me.II.BackColor = System.Drawing.Color.RoyalBlue
+        Me.II.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.II.Location = New System.Drawing.Point(221, 247)
+        Me.II.Name = "II"
+        Me.II.Size = New System.Drawing.Size(26, 101)
+        Me.II.TabIndex = 3
+        '
+        'VII
+        '
+        Me.VII.BackColor = System.Drawing.Color.RoyalBlue
+        Me.VII.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.VII.Location = New System.Drawing.Point(93, 247)
+        Me.VII.Name = "VII"
+        Me.VII.Size = New System.Drawing.Size(26, 101)
+        Me.VII.TabIndex = 4
+        Me.VII.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'I
+        '
+        Me.I.BackColor = System.Drawing.Color.RoyalBlue
+        Me.I.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.I.Location = New System.Drawing.Point(223, 115)
+        Me.I.Name = "I"
+        Me.I.Size = New System.Drawing.Size(26, 101)
+        Me.I.TabIndex = 5
+        '
+        'IV
+        '
+        Me.IV.BackColor = System.Drawing.Color.White
+        Me.IV.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.IV.Location = New System.Drawing.Point(120, 219)
+        Me.IV.Name = "IV"
+        Me.IV.Size = New System.Drawing.Size(101, 26)
+        Me.IV.TabIndex = 6
+        '
+        'V
+        '
+        Me.V.BackColor = System.Drawing.Color.RoyalBlue
+        Me.V.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.V.Location = New System.Drawing.Point(120, 354)
+        Me.V.Name = "V"
+        Me.V.Size = New System.Drawing.Size(101, 26)
+        Me.V.TabIndex = 7
+        '
+        'III
+        '
+        Me.III.BackColor = System.Drawing.Color.RoyalBlue
+        Me.III.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.III.Location = New System.Drawing.Point(120, 87)
+        Me.III.Name = "III"
+        Me.III.Size = New System.Drawing.Size(101, 26)
+        Me.III.TabIndex = 8
+        Me.III.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.PaleGreen
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(32, 517)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 37)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "START"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(209, 517)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(106, 37)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "STOP"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 1.0!)
+        Me.Label2.Location = New System.Drawing.Point(145, 587)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(3, 2)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(357, 613)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.III)
+        Me.Controls.Add(Me.V)
+        Me.Controls.Add(Me.IV)
+        Me.Controls.Add(Me.I)
+        Me.Controls.Add(Me.VII)
+        Me.Controls.Add(Me.II)
+        Me.Controls.Add(Me.VI)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents VI As Label
+    Friend WithEvents II As Label
+    Friend WithEvents VII As Label
+    Friend WithEvents I As Label
+    Friend WithEvents IV As Label
+    Friend WithEvents V As Label
+    Friend WithEvents III As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
 End Class
--- 7Segment-Countdown/Form1.vb
+++ 7Segment-Countdown/Form1.vb
@@ -1,3 +1,113 @@
 ﻿Public Class Form1
 
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles I.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label2.Text += 1
+        If Label2.Text = 0 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.White
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.RoyalBlue
+        ElseIf Label2.Text = 1 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.White
+            IV.BackColor = Color.White
+            V.BackColor = Color.White
+            VI.BackColor = Color.White
+            VII.BackColor = Color.White
+        ElseIf Label2.Text = 2 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.White
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.White
+            VII.BackColor = Color.RoyalBlue
+        ElseIf Label2.Text = 3 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.White
+            VII.BackColor = Color.White
+        ElseIf Label2.Text = 4 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.White
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.White
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.White
+        ElseIf Label2.Text = 5 Then
+            I.BackColor = Color.White
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.White
+        ElseIf Label2.Text = 6 Then
+            I.BackColor = Color.White
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.RoyalBlue
+        ElseIf Label2.Text = 7 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.White
+            V.BackColor = Color.White
+            VI.BackColor = Color.White
+            VII.BackColor = Color.White
+        ElseIf Label2.Text = 8 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.RoyalBlue
+        ElseIf Label2.Text = 9 Then
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.RoyalBlue
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.White
+        Else
+            Label2.Text = 0
+            I.BackColor = Color.RoyalBlue
+            II.BackColor = Color.RoyalBlue
+            III.BackColor = Color.RoyalBlue
+            IV.BackColor = Color.White
+            V.BackColor = Color.RoyalBlue
+            VI.BackColor = Color.RoyalBlue
+            VII.BackColor = Color.RoyalBlue
+        End If
+    End Sub
 End Class
